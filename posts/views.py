a983278f6d69ae2995aef1f071df89c1b76d4347
Eunjeong Park <wandukong@snu.ac.kr>
2019-04-10 07:23:00 +0000

    :sparkles: Make url and action of 'posts:create'

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
 
 def create(request):
-    pass
+    if request.method == 'POST':
+        # 작성된 post를 DB에 적용
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:create')
+    else: # GET
+        # post를 작성하는 form을 보여줌
+        form = PostForm()
+        return render(request, 'posts/create.html', {
+            'form': form,
+        })
+        
