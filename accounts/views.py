af9cfd7ef044dbddcf09e2f485db07d0365f14f1
Eunjeong Park <wandukong@snu.ac.kr>
2019-04-15 04:41:48 +0000

    :sparkles: Implement signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def login(request):
     # POST : 실제 로그인 (세션에 유저 정보 추가)
@@ -16,9 +16,22 @@ def login(request):
         # login_form = LoginForm()
         form = AuthenticationForm()
         return render(request, 'accounts/login.html', { 
-            'form' : form,
+            'form': form,
         })
         
 def logout(request):
     auth_logout(request)
     return redirect('posts:feed')
+    
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('posts:feed')
+    else:
+        form = UserCreationForm()
+        return render(request, 'accounts/signup.html', { 
+            'form': form,
+        })
