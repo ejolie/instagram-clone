3a1d1c3967742ce4453407e9a3f71e8ead07c1d1
Eunjeong Park <wandukong@snu.ac.kr>
2019-04-16 01:57:56 +0000

    :hammer: Change the word 'form' to more detailed word

--- accounts/views.py
+++ accounts/views.py
@@ -5,18 +5,18 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 def login(request):
     # POST : 실제 로그인 (세션에 유저 정보 추가)
     if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            auth_login(request, form.get_user())
+        login_form = AuthenticationForm(request, request.POST)
+        if login_form.is_valid():
+            auth_login(request, login_form.get_user())
             # next
             #
             return redirect(request.GET.get('next') or 'posts:feed')
     # GET : 로그인 정보 입력
     else:
         # login_form = LoginForm()
-        form = AuthenticationForm()
+        login_form = AuthenticationForm()
         return render(request, 'accounts/login.html', { 
-            'form': form,
+            'login_form': login_form,
         })
         
 def logout(request):
@@ -25,15 +25,15 @@ def logout(request):
     
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
+        signup_form = UserCreationForm(request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save()
             auth_login(request, user)
             return redirect('posts:feed')
     else:
-        form = UserCreationForm()
+        signup_form = UserCreationForm()
         return render(request, 'accounts/signup.html', { 
-            'form': form,
+            'signup_form': signup_form,
         })
         
 def profile(request, username):
